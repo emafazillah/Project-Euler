e516f0362d3be8510bdc607c7c5a85cbe89f5a0a
Ema Fazillah Abu Bakar <gatsu2811@gmail.com>
2017-06-13 12:54:11 +0800

    Add Largest Prime Factor

--- src/difficulty005/LargestPrimeFactor.java
+++ src/difficulty005/LargestPrimeFactor.java
@@ -1,17 +1,32 @@
 package difficulty005;
 
-import java.math.BigInteger;
-
 public class LargestPrimeFactor {
 	
-	// nanti sambung
 	public static void main(String[] args) {
-		BigInteger input = new BigInteger("13195"); // 600851475143		
-		long biggest_prime_factor = input.longValue();
-		while(input.isProbablePrime(1) && (input.longValue() % biggest_prime_factor == 0)) {	
-			biggest_prime_factor--;
-		}
-		System.out.print(biggest_prime_factor);
+		long input = 600851475143L;
+		long temp = 4L;
+		long factoral = 0L;
+		boolean isBiggestPrimeFactor = false;		
+		while(!isBiggestPrimeFactor) {
+			// Sieve of Eratosthene or Trial division		
+			if(input % 2L != 0 && input % 3L != 0) {
+				while(temp * temp <= input) {
+					if(input % temp == 0L) {
+						if(factoral == 0L) 
+							factoral = temp;
+						else 
+							factoral *= temp;
+						break;
+					}
+					temp++;
+				}
+			}
+			if(factoral == input) {
+				isBiggestPrimeFactor = true;
+				System.out.print(temp);
+			}
+			temp++;
+		}		
 	}
 
 }
